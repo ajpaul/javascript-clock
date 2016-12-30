0331ff4cd1143acd4379157a8b9a049e6ccc2ffa
Adam Paul <ajp01@acuitysso.com>
2016-12-30 11:47:06 -0500

    Updated styles and layout

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,72 @@ var time = function() {
     this.hour = this.now.getHours();
 
     //hours are in military time. I don't want that
-    this.hours = (this.hours > 12) ? this.hours - 12 : this.hours;
+    this.hour = (this.hour > 12) ? this.hour - 12 : this.hour;
     this.analogShowing = false;
-}
+};
 
 time.prototype.getTime = function() {
     this.now = new Date();
     var current = this.now.toLocaleTimeString();
     document.querySelector(".digital-clock").innerHTML = current;
-}
+};
 
 time.prototype.dissectTime = function() {
     this.now = new Date();
     console.log(this.hour + ", " + this.minutes + ', ' + this.seconds);
-}
+};
 
+//how do I call this from the html?
 time.prototype.toggle = function() {
-    alert("toggle");
-}
 
-var lol = new time();
-lol.getTime();
-lol.dissectTime();
-console.log(lol);
+    console.log("toggled");
+
+    var dc = document.getElementById("digital");
+    var ac = document.getElementById("analog");
+
+    if(rightNow.analogShowing) { //show dc
+        ac.style.transform = "translateX(1000px)";
+        dc.style.transform = "translateX(0px)";
+
+        changeDisplay(ac, false, 300);
+        changeDisplay(dc, true, 100);
+ 
+    }
+    else { //show ac
+        ac.style.transform = "translateX(0px)";
+        dc.style.transform = "translateX(-1000px)";
+
+        changeDisplay(dc, false, 300);
+        changeDisplay(ac, true, 100);
+ 
+    }
+
+    rightNow.analogShowing = !rightNow.analogShowing;
+};
+
+
+//driver code
+var rightNow = new time();
+rightNow.getTime();
+rightNow.dissectTime();
+console.log(rightNow);
 
 window.setInterval(function() {
-    lol.getTime();
+    rightNow.getTime();
 }, 1000);
 
-function toggle() {
-    if(lol.analogShowing)
-        alert("analogShowing");
-    else 
-        alert("digital showing");
+var toggleBtns = document.querySelectorAll(".button");
+
+for (var i = 0; i < toggleBtns.length; i++) {
+    toggleBtns[i].addEventListener('click', rightNow.toggle(), false);
+}
+
+function changeDisplay(element, bool, timeout) {
+    
+    setTimeout(function(element, bool) {
+        if (bool) 
+            element.style.display = "flex";
+        //else
+            //element.style.display = "none";
+    }, timeout);
 }
